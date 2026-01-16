e86395a3eca4d5a13d1396a59f319b6b1ab86108
calypsoidea <calipsoideaoooi@gmail.com>
2026-01-16 15:24:41 -0300

    dfdas

--- workspace/js-hardhat-project/scripts-version20/scripts/LP-Studies/yalps03.ts
+++ workspace/js-hardhat-project/scripts-version20/scripts/LP-Studies/yalps03.ts
@@ -6,18 +6,18 @@ import { lessEq, equalTo, greaterEq, inRange } from "yalps"
 import { Model, Constraint, Coefficients, OptimizationDirection, Options, Solution } from "yalps"
 
 const model = {
-    direction: "maximize" as const,
+    direction: "minimize" as const,
     objective: "profit",
     constraints: {
-      first: { max: -700 },
-      second: { max: -400 }, // labor should be <= 110
+      first: { min: 700 },
+      second: { min: 400 }, // labor should be <= 110
     },
     variables: {
-      x1: { first: -1, second: 0, profit: -2 },
-      x2: { first: 0, second: -1, profit: -20 },
-      x3: { first: -1, second: 0, profit: -1 },
-      x4: { first: -1, second: -1, profit: -11 },
-      x5: { first: -2, second: -1, profit: -12 },
+      x1: { first: 1, second: 0, profit: 2 },
+      x2: { first: 0, second: -1, profit: 20 },
+      x3: { first: 1, second: 0, profit: 1 },
+      x4: { first: 1, second: 1, profit: 11 },
+      x5: { first: 2, second: 1, profit: 12 },
     },
     //integers: ["table", "dresser"], // these variables must have an integer value in the solution
     integers: false,
